04ce7919016d0dd5fe3acdff051abe363d262f93
abir yusuf <yusuf_ctg@yahoo.com>
2019-03-27 09:00:48 -0400

    "JDBC"

---
+++ src/test/java/selenium_practice/JDBCTest.java
@@ -0,0 +1,25 @@
+package selenium_practice;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class JDBCTest {
+    public static void main(String[] args) {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test2","root","Abir123");
+            // database name, root is username and password
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from orders");
+
+            while (rs.next())
+                System.out.println(rs.getInt(1) + "  " + rs.getString(2));
+            con.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
