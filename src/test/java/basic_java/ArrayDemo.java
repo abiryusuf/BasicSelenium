89bb1f11e484999a61ffbff0558d2a4e84d6f7c2
abir yusuf <yusuf_ctg@yahoo.com>
2019-03-27 23:39:16 -0400

    "Array"

--- src/test/java/basic_java/ArrayDemo.java
+++ src/test/java/basic_java/ArrayDemo.java
@@ -3,15 +3,41 @@ package basic_java;
 public class ArrayDemo {
 
     public static void main(String[] args) {
-        int a[]={1,2,3,4};
+     Method1();
+     FindNumber();
+
+        }
+
+        public static void Method1() {
+            int a[] = {1, 2, 3, 4};
       /*  int a[] = new int[5]; //declaration
         a[0] = 1; //initializing arrays
         a[1] = 2;
         a[2] = 3;
         a[3] = 4;*/
-       // a[4]=5;
-        for (int i=0; i<a.length; i++) { //length of property of array
-            System.out.println(a[i]); //printing out values of array
+            // a[4]=5;
+            for (int i = 0; i < a.length; i++) { //length of property of array
+                System.out.println(a[i]); //printing out values of array
+            }
         }
+        public static void FindNumber(){
+
+                int intArray[]=new int[]{30,20,10,50,60};
+                int largest=Integer.MIN_VALUE;
+                int smallest=Integer.MAX_VALUE;
+                for (int number : intArray){
+                    if (number>largest){
+                        largest=number;
+                    }
+                   // System.out.println(largest);
+                }  System.out.println(largest);
+
+                for (int number : intArray){
+                    if (number<smallest){
+                        smallest=number;
+                    }
+                }
+            System.out.println(smallest);
+            }
     }
-}
+
