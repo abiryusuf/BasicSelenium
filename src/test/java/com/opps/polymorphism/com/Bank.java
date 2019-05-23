9fca4d6349bbfe9ab4bdf5cbe1537d0b0d6afdf7
abir yusuf <yusuf_ctg@yahoo.com>
2019-05-23 02:36:17 -0400

    "Polymorphism"

--- src/test/java/com/opps/polymorphism/com/Bank.java
+++ src/test/java/com/opps/polymorphism/com/Bank.java
@@ -1,9 +1,13 @@
 package com.opps.polymorphism.com;
 
 public class Bank {
-//A single action in different ways, for example;
+//A single action in different ways it's call polymorphism, for example;
+//Polymorphism allows you define one interface and have multiple implementations.
     public void InterestRate(){
         System.out.println("Bank interest rate is not name ");
 
+    }
+    public void savingAccount(){
+        System.out.println("saving account");
     }
 }
