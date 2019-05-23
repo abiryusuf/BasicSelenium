b50b27478e332afbad36e0cbc5361d2ff1805139
abir yusuf <yusuf_ctg@yahoo.com>
2019-05-23 02:36:49 -0400

    "Polymorphism"

--- src/test/java/com/opps/polymorphism/com/CitiBank.java
+++ src/test/java/com/opps/polymorphism/com/CitiBank.java
@@ -1,15 +1,21 @@
 package com.opps.polymorphism.com;
 
 public class CitiBank extends Bank {
-
+//runtime polymorphism is related with overriding
     @Override
     public void InterestRate(){
         System.out.println("10%");
     }
+    @Override
+    public void savingAccount(){
+        System.out.println(10);
+    }
+
 
 
     public static void main(String[] args) {
         CitiBank rate = new CitiBank();
         rate.InterestRate();
+        rate.savingAccount();
     }
 }
